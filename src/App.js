3084bd7c45e3b81c942119cbb4c88d86cd297776
Ankita196 <muskanmakde1999@gmail.com>
2021-08-21 22:32:48 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ export default function App() {
   const APP_KEY = '3c6d7c8c19e2ec9e1047c7771953ca32';
   const [recipe, setResipe] = useState([]);
   const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
   useEffect(() => {
     getRecipe();
-  }, []);
+  }, [query]);
 
   const getRecipe = async () => {
     const response = await axios.get(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
     );
     setResipe(response.data.hits);
     console.log(response.data.hits);
@@ -22,13 +23,16 @@ export default function App() {
 
   const updateSearch = e => {
     setSearch(e.target.value);
-    console.log(e.target.value);
+  };
+  const updateQuery = e => {
+    e.preventDefault();
+    setQuery(search);
   };
   return (
     <div className="App">
-      <form>
+      <form onSubmit={updateQuery}>
         <input type="text" />
-        <button type="button" value={search} onChange={updateSearch}>
+        <button type="submt" value={search} onChange={updateSearch}>
           Search
         </button>
       </form>
