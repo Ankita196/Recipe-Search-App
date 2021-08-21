c5f3b421bf55ab72dcfda1c5eeffe36dc334f632
Ankita196 <muskanmakde1999@gmail.com>
2021-08-21 19:33:52 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
-import "./style.css";
+mport React, { useEffect } from 'react';
+import './style.css';
+import axios from "axios"
 
 export default function App() {
+  const APP_ID = '4b69527e';
+  const APP_KEY = '3c6d7c8c19e2ec9e1047c7771953ca32	—';
+  useEffect(() => {
+    console.log('effect runs');
+  }, []);
+
+   const getRecipe= async ()=>{
+const response =await axios.get()
+   }
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <form>
+        <input type="text" />
+        <button type="button">Search</button>
+      </form>
     </div>
   );
 }
