16158a0d433ef765d075293ce214616cf3f31e8d
Ankita196 <muskanmakde1999@gmail.com>
2021-08-22 12:41:28 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -11,7 +11,28 @@ import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import DirectionsIcon from '@material-ui/icons/Directions';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1
+  },
+  iconButton: {
+    padding: 10
+  },
+  divider: {
+    height: 28,
+    margin: 4
+  }
+}));
+
 export default function App() {
+  const classes = useStyles();
   const APP_ID = '4b69527e';
   const APP_KEY = '3c6d7c8c19e2ec9e1047c7771953ca32';
   const [recipe, setResipe] = useState([]);
@@ -38,6 +59,20 @@ export default function App() {
   }
   return (
     <div className="App">
+      <Paper component="form" className={classes.root} onSubmit={updateQuery}>
+        <InputBase
+          className={classes.input}
+          placeholder="Search for recipe"
+          inputProps={{ 'aria-label': 'search for recipe' }}
+        />
+        <IconButton
+          type="submit"
+          className={classes.iconButton}
+          aria-label="search"
+        >
+          <SearchIcon />
+        </IconButton>
+      </Paper>
       <form onSubmit={updateQuery}>
         <input type="text" value={search} onChange={updateSearch} />
         <button type="submit">Search</button>
