b2042dadbed6685fce415c0a850a57187f0d7291
Ankita196 <muskanmakde1999@gmail.com>
2021-09-07 13:41:38 +0530

    1 file modified

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Link} from "react-router-dom"
+import { Link } from 'react-router-dom';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -22,26 +22,23 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
 
-
-
-const styles = (theme) => ({
+const styles = theme => ({
   root: {
     margin: 0,
-    padding: theme.spacing(2),
+    padding: theme.spacing(2)
   },
   closeButton: {
     position: 'absolute',
     right: theme.spacing(1),
     top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
+    color: theme.palette.grey[500]
+  }
 });
 const useStyles = makeStyles(theme => ({
   root: {
-    width: 300,
-    
+    width: 300
   },
-  
+
   media: {
     marginRight: 'auto',
     marginLeft: 'auto'
@@ -58,35 +55,37 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-
-const DialogTitle = withStyles(styles)((props) => {
+const DialogTitle = withStyles(styles)(props => {
   const { children, classes, onClose, ...other } = props;
   return (
     <MuiDialogTitle disableTypography className={classes.root} {...other}>
       <Typography variant="h6">{children}</Typography>
       {onClose ? (
-        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+        >
           <CloseIcon />
         </IconButton>
       ) : null}
     </MuiDialogTitle>
   );
 });
-const DialogContent = withStyles((theme) => ({
+const DialogContent = withStyles(theme => ({
   root: {
-    padding: theme.spacing(2),
-  },
+    padding: theme.spacing(2)
+  }
 }))(MuiDialogContent);
 
-const DialogActions = withStyles((theme) => ({
+const DialogActions = withStyles(theme => ({
   root: {
     margin: 0,
-    padding: theme.spacing(1),
-  },
+    padding: theme.spacing(1)
+  }
 }))(MuiDialogActions);
 
-
-const Recipe = ({ title, calories, image, ingredients ,mealType}) => {
+const Recipe = ({ title, calories, image, ingredients, mealType }) => {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
 
@@ -97,154 +96,67 @@ const Recipe = ({ title, calories, image, ingredients ,mealType}) => {
     setOpen(false);
   };
 
-
-  
-
   return (
     <div>
       <Card className={classes.root}>
-        <CardHeader
-         className={classes.media}
-          title={title}
-          
-        />
-        <Typography style={{marginLeft:30}}>Calories :{calories}</Typography>
+        <CardHeader className={classes.media} title={title} />
+        <Typography style={{ marginLeft: 30 }}>Calories :{calories}</Typography>
         <img
-                      src={image}
-                      style={{ height: '75%', width: '75%', display: 'block' }}
-                      className={classes.media}
-                    />
-        <div style={{display:"flex", justifyContent: 'center',}}> 
-        <CardContent >
-      
-        <div>
-//       <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-//         Open dialog
-//       </Button>
-//       <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
-//         <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-//           Modal title
-//         </DialogTitle>
-//         <DialogContent dividers>
-//           <Typography gutterBottom>
-//             Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-//             in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-//           </Typography>
-//           <Typography gutterBottom>
-//             Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-//             lacus vel augue laoreet rutrum faucibus dolor auctor.
-//           </Typography>
-//           <Typography gutterBottom>
-//             Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
-//             scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
-//             auctor fringilla.
-//           </Typography>
-//         </DialogContent>
-//         <DialogActions>
-//           <Button autoFocus onClick={handleClose} color="primary">
-//             Save changes
-//           </Button>
-//         </DialogActions>
-//       </Dialog>
-//     </div> <br/> <br/>
-<div>
-//       <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-//         Open dialog
-//       </Button>
-//       <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
-//         <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-//           Modal title
-//         </DialogTitle>
-//         <DialogContent dividers>
-//           <Typography gutterBottom>
-//             Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-//             in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-//           </Typography>
-//           <Typography gutterBottom>
-//             Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-//             lacus vel augue laoreet rutrum faucibus dolor auctor.
-//           </Typography>
-//           <Typography gutterBottom>
-//             Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
-//             scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
-//             auctor fringilla.
-//           </Typography>
-//         </DialogContent>
-//         <DialogActions>
-//           <Button autoFocus onClick={handleClose} color="primary">
-//             Save changes
-//           </Button>
-//         </DialogActions>
-//       </Dialog>
-//     </div>
-        </CardContent>
-       </div>
-            {/* {mealType.map(mealtype => (
-              <Typography
-                variant="body2"
-                color="textSecondary"
-                component="p"
-                paragraph
+          src={image}
+          style={{ height: '75%', width: '75%', display: 'block' }}
+          className={classes.media}
+        />
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <CardContent>
+            <div>
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ width: 250 }}
+                color="primary"
+                onClick={handleClickOpen}
               >
-              {' '}
-             {mealType.source}
-              </Typography>
-            ))} */}
-           
-      
-          {/* <CardContent>
-            {ingredients.map(ingredient => (
-              <Typography
-                variant="body2"
-                color="textSecondary"
-                component="p"
-                paragraph
+                Recipe
+              </Button>
+              <Dialog
+                onClose={handleClose}
+                aria-labelledby="customized-dialog-title"
+                open={open}
               >
-                {' '}
-                {ingredient.text}
-              </Typography>
-            ))}
+                <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+                  {title}
+                </DialogTitle>
+                <DialogContent dividers>
+                  <CardContent>
+                    {ingredients.map(ingredient => (
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                        paragraph
+                      >
+                        {' '}
+                        {ingredient.text}
+                      </Typography>
+                    ))}
+                  </CardContent>
+                </DialogContent>
+                <DialogActions>
+                  <Button autoFocus onClick={handleClose} color="primary">
+                    close
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            </div>{' '}
+            <br /> <br />
+            <Button variant="contained" color="primary" style={{ width: 250 }}>
+              Ingrediants
+            </Button>
           </CardContent>
-        */}
+        </div>
       </Card>
     </div>
   );
 };
 
-
-
-export default Recipe
-
-//   return (
-//     <div>
-//       <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-//         Open dialog
-//       </Button>
-//       <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
-//         <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-//           Modal title
-//         </DialogTitle>
-//         <DialogContent dividers>
-//           <Typography gutterBottom>
-//             Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-//             in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-//           </Typography>
-//           <Typography gutterBottom>
-//             Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-//             lacus vel augue laoreet rutrum faucibus dolor auctor.
-//           </Typography>
-//           <Typography gutterBottom>
-//             Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
-//             scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
-//             auctor fringilla.
-//           </Typography>
-//         </DialogContent>
-//         <DialogActions>
-//           <Button autoFocus onClick={handleClose} color="primary">
-//             Save changes
-//           </Button>
-//         </DialogActions>
-//       </Dialog>
-//     </div>
-//   );
-// }
+export default Recipe;
